9bd661296970b6e7a95b23151b1e4afc0aebf73a
luxu-ms <luxu@microsoft.com>
2026-06-01 13:59:28 +0800

    Task 003: Upgrade System.Data.SqlClient to Microsoft.Data.SqlClient
    - Added Microsoft.Data.SqlClient 5.2.2 and Microsoft.EntityFramework.SqlServer 6.5.2 packages
    - Replaced EntityFramework.SqlServer reference with Microsoft.EntityFramework.SqlServer
    - Updated entityFramework provider in Web.config: invariantName=Microsoft.Data.SqlClient, MicrosoftSqlProviderServices
    - Added system.data DbProviderFactories section for Microsoft.Data.SqlClient registration
    - Updated connection string providerName to Microsoft.Data.SqlClient with Authentication=Active Directory Default
    - Added [DbConfigurationType(typeof(MicrosoftSqlDbConfiguration))] to eShopContext
    - Updated Web.Release.config connection string providerName to Microsoft.Data.SqlClient

--- eShopLegacy/DAL/eShopContext.cs
+++ eShopLegacy/DAL/eShopContext.cs
@@ -1,9 +1,15 @@
 using System.Data.Entity;
+using System.Data.Entity.SqlServer;
 using Microsoft.AspNet.Identity.EntityFramework;
 using eShopLegacy.Models;
 
 namespace eShopLegacy.DAL
 {
+    /// <summary>
+    /// EF6 database context configured to use Microsoft.Data.SqlClient via MicrosoftSqlDbConfiguration.
+    /// This enables Azure AD Managed Identity authentication for Azure SQL Database.
+    /// </summary>
+    [DbConfigurationType(typeof(MicrosoftSqlDbConfiguration))]
     public class eShopContext : IdentityDbContext<ApplicationUser>
     {
         public eShopContext()
